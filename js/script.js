8058516d19b92361d2672b7de166c6d5b2782cbe
Joe-singh1993 <mherfrangulyan1987@gmail.com>
2022-06-15 03:28:11 +0400

    new-changes 15.06.22

--- js/script.js
+++ js/script.js
@@ -34,3 +34,37 @@ window.onscroll = function(){
 $('.navbar__btn.upbtn').on('click', function(){
     $('html, body').animate({scrollTop: 0});
 })
+
+var popup = document.querySelectorAll('.popup-menu');
+if(popup){
+    popup.forEach(function(key, value){
+        key.onclick = function(){
+           var dropdown = key.parentNode.querySelector('.dropdown');
+           if(!dropdown.classList.contains('open')){
+            dropdown.classList.add('open');
+           }else{
+            dropdown.classList.remove('open');
+           }
+        }
+    })
+}
+var cl = document.querySelectorAll('.dropdown .close_btn');
+if(cl){
+    cl.forEach(function(key, value){
+        key.onclick = function(){
+            if(key.parentNode.classList.contains('open')){
+               key.parentNode.classList.remove('open'); 
+            }
+        }
+    })
+}
+
+
+var smallpics = document.querySelectorAll('.smallpics img');
+if(smallpics){
+    smallpics.forEach(function(key, value){
+        key.onclick = function(){
+           document.querySelector('.bigpic').setAttribute('src', key.getAttribute('src'));
+        }
+    })
+}
